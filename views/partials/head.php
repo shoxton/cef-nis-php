70ea7ddaabd62cf8c174edbeed4d9334cb4f8c97
André Pereira <andrepereirapink@gmail.com>
2023-07-04 12:34:14 -0300

    adjust link

--- views/partials/head.php
+++ views/partials/head.php
@@ -9,14 +9,14 @@
 </head>
 
 <body>
-    <header class=" py-5 mb-5 border-b">
+    <header class="px-3 py-5 mb-5 border-b">
         <div class="container mx-auto flex items-center justify-between">
             <a href="/" class="text-sm font-bold text-blue-900 hover:text-blue-600">NIS - PHP</a>
 
             <nav>
                 <ul class="flex gap-3">
                     <li><a class="text-blue-600 font-medium p-3 hover:bg-blue-50" href="/cadastro">Novo cadastro</a></li>
-                    <li><a class="text-blue-600 font-medium p-3 hover:bg-blue-50" href="/">Consultar NIS</a></li>
+                    <li><a class="text-blue-600 font-medium p-3 hover:bg-blue-50" href="/consulta">Consultar NIS</a></li>
                 </ul>
             </nav>
         </div>
